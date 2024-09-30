dff278f1a33eefd8fd6e63d13fcdf3888c10de9f
Eli OCCANSEY <62498537+elioccansey@users.noreply.github.com>
2024-09-30 20:09:25 +0000

    two sum

---
+++ two-sum.java
@@ -0,0 +1,8 @@
+public int[] twoSum(int[] nums, int target){
+    var map = new HashMap<Integer>();
+    for(int i = 0; i < nums.length; i++){
+        int key = target - nums[i];
+        if(map.containsKey(key)) return new int[] {i, key};
+        map.put(nums[i], i);
+    }
+}
